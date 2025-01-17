3e6e04126e18aea4018a39c8221df21ccefa134d
Pratham Singh <prathambsingh891@gmail.com>
2025-01-17 16:02:46 +0530

    update product management controller to include status codes in responses

--- controllers/productManagement.js
+++ controllers/productManagement.js
@@ -4,7 +4,7 @@ import Product from '../models/Product.js';
 export const getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.json(products);
+    res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -17,7 +17,7 @@ export const getProductById = async (req, res) => {
     if (!product) {
       return res.status(404).json({ message: 'Product not found' });
     }
-    res.json(product);
+    res.status(200).json(product);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
