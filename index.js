bd939bca8356835e49aee31a64513a5f17ce6f93
Pratham Singh <prathambsingh891@gmail.com>
2025-04-04 00:09:43 +0530

    add health check endpoint; implement root route to confirm API is running

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
 app.use(express.json());
 app.use('/api', router);
 
